2829fafc176cbb9fbfcde9f46edc74603ff823c9
kelby <kelbytansley.kt@gmail.com>
2023-01-12 08:25:19 +0200

    SEO and performance adjustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,19 +9,19 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'home', title: 'Home', component: HomeComponent },
-  { path: 'accommodation', title: 'Accommodation', component: AccommodationComponent },
+  { path: 'home', title: 'Spinning Reel Beach Cottages', component: HomeComponent },
+  { path: 'accommodation', title: 'Spinning Reel Accommodation', component: AccommodationComponent },
   { path: 'accommodation/beach-cottages', title: 'Beach Cottage Accommodation', component: BeachCottagesComponent },
   { path: 'accommodation/log-chalets', title: 'Log Chalet Accommodation', component: ChaletsComponent },
   { path: 'accommodation/vermont-chalet', title: 'Vermont Chalet Accommodation', component: VermontComponent },
   { path: 'accommodation/bed-and-breakfast', title: 'Bed and Breakfast Accommodation', component: BedAndBreakfastComponent },
-  { path: 'contact', title: 'Contact', component: ContactComponent },
+  { path: 'contact', title: 'Contact Spinning Reel', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', title: 'Home', component: HomeComponent },
+  { path: '**', title: 'Spinning Reel Beach Cottages', component: HomeComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
